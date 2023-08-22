3a95408c8fe79c646663913b92443186fbd4000d
Jancsó Anita <anita.jancso@webapix.hu>
2023-08-22 09:45:20 +0200

    create project

---
+++ app/Models/Project.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function contacts(): BelongsToMany
+    {
+        $this->belongsToMany(Contact::class, 'contact_project');
+    }
+}
---
+++ config/statuses.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'project_status' =>[
+        'todo' => 'Fejlesztésre vár',
+        'in_progress' => 'Folyamatban',
+        'done' => 'Kész',
+    ],
+];
